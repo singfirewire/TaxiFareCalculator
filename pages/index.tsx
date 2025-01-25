f3ea08e3642585573136af0cb70bf7ebc98ae4a5
singfirewire <84568649+singfirewire@users.noreply.github.com>
2025-01-25 19:20:56 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import TaxiFareCalculator from '../components/TaxiFareCalculator';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <TaxiFareCalculator />
+    </div>
+  );
+}
