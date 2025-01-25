74d45facbc12661f5da8470e94e91f3dbc23976d
singfirewire <84568649+singfirewire@users.noreply.github.com>
2025-01-25 19:26:28 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TaxiFareCalculator from './components/TaxiFareCalculator'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <TaxiFareCalculator />
+    </div>
+  )
+}
+
+export default App
